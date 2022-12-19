5332f1ec08235faf78328f6cf2939dbd32af483a
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-12-19 10:29:36 +0100

    Chore(Test) Remove publish message for cucumber

--- cucumber.js
+++ cucumber.js
@@ -4,6 +4,7 @@ let common = [
     '--require-module ts-node/register',    // Load TypeScript module
     '--require testbdd/step-definitions/**/*.ts',   // Load step definitions
     '--format progress-bar',                // Load custom formatter
+    '--publish-quiet'
   ].join(' ');
   
 module.exports = {
